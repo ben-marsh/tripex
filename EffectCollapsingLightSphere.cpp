681d817e2d812ebd0527c35e85b0638d48b229ad
Ben Marsh <6538947+ben-marsh@users.noreply.github.com>
2025-01-20 08:14:21 -0500

    Coding style for collapsinglightsphere

--- EffectCollapsingLightSphere.cpp
+++ EffectCollapsingLightSphere.cpp
@@ -6,15 +6,6 @@
 #include "TextureData.h"
 #include <algorithm>
 
-//static ZObject *pObj;
-
-//double dRadAng = 2 * 3.14159 * 2.0;
-//double dViewAng = 0;
-//int nSpikes = 8;
-//Texture *pTint;
-
-//#define alNumLS 512
-
 class EffectCollapsingLightSphere : public Effect
 {
 public:
@@ -37,14 +28,14 @@ public:
 	const float SPEEDSPREAD = 20;
 	const float FOREGROUNDBR = 0.5f;
 
-	double pdAng[SOURCES];
-	double pdYPos[SOURCES];
-	double pdTilt[SOURCES];
-	double pdRadius[SOURCES];
-	double pdSpeed[SOURCES];
-	Texture *pTint;
-	double dWaitTime;
-	double dTilt;
+	double source_ang[SOURCES];
+	double source_y_pos[SOURCES];
+	double source_tilt[SOURCES];
+	double source_radius[SOURCES];
+	double source_speed[SOURCES];
+	Texture *tint = nullptr;
+	double wait_time;
+	double tilt;
 	Actor obj;
 	Camera camera;
 
@@ -52,18 +43,17 @@ public:
 	// 1 - moving out
 	// 2 - out
 	// 3 - moving in
-	int nStage;
-	double brt;
+	int current_stage;
+	double brt = 0.0;
 
 	EffectCollapsingLightSphere()
 		: Effect({ &sprite_texture_class, &tint_texture_class })
 	{
-		dWaitTime = 0;
-		dTilt = 0;
+		wait_time = 0;
+		tilt = 0;
 
-		nStage = 0;
+		current_stage = 0;
 		obj.vertices.resize(SOURCES);
-	//	pObj->Create(SOURCES, 1);
 		obj.flags.set( Actor::F_DRAW_TRANSPARENT );
 		obj.flags.set( Actor::F_DRAW_VERTEX_SPRITES );
 		obj.exposure_light_delta = WideColorRgb(-60, -60, -60);
@@ -74,59 +64,58 @@ public:
 
 		for(int i = 0; i < SOURCES; i++)
 		{
-			pdAng[i] = rand() * PI2 / RAND_MAX;
-			pdSpeed[i] = SPEEDSPREAD * (rand() * (SPEEDSPREAD / 2.0) * DEG_TO_RAD / RAND_MAX);
-			pdTilt[i] = rand() * PI2 / RAND_MAX;
-			pdRadius[i] = rand() * RADIUS / RAND_MAX;
-			pdYPos[i] = (SPREAD / 2.0) - (double(rand()) * SPREAD / RAND_MAX);
+			source_ang[i] = rand() * PI2 / RAND_MAX;
+			source_speed[i] = SPEEDSPREAD * (rand() * (SPEEDSPREAD / 2.0) * DEG_TO_RAD / RAND_MAX);
+			source_tilt[i] = rand() * PI2 / RAND_MAX;
+			source_radius[i] = rand() * RADIUS / RAND_MAX;
+			source_y_pos[i] = (SPREAD / 2.0) - (double(rand()) * SPREAD / RAND_MAX);
 		}
 	}
 	Error* Calculate(const CalculateParams& params) override
 	{
 		brt = params.brightness;
-		if(nStage == 0) dWaitTime += params.elapsed * params.audio_data.GetDampenedBand( sensitivity, 0.0f, 1.0f);
-		else if(nStage == 1) dTilt += params.elapsed * 1.5 * DEG_TO_RAD;
-		else if(nStage == 2) dWaitTime += params.elapsed * params.audio_data.GetDampenedBand(sensitivity, 0.0f, 1.0f);
-		else if(nStage == 3) dTilt -= params.elapsed * 1.5 * DEG_TO_RAD;
+		if(current_stage == 0) wait_time += params.elapsed * params.audio_data.GetDampenedBand( sensitivity, 0.0f, 1.0f);
+		else if(current_stage == 1) tilt += params.elapsed * 1.5 * DEG_TO_RAD;
+		else if(current_stage == 2) wait_time += params.elapsed * params.audio_data.GetDampenedBand(sensitivity, 0.0f, 1.0f);
+		else if(current_stage == 3) tilt -= params.elapsed * 1.5 * DEG_TO_RAD;
 
-		if((dTilt < 0) || (dTilt > 3.141592 / 2.0) || (dWaitTime > 40.0 && params.audio_data.GetIntensity( ) > 0.6))
+		if((tilt < 0) || (tilt > 3.141592 / 2.0) || (wait_time > 40.0 && params.audio_data.GetIntensity( ) > 0.6))
 		{
-			nStage = (nStage + 1) & 3;
-			dWaitTime = 0.0;
-			dTilt = std::min(std::max(dTilt, 0.0), 3.14159 / 2.0);
+			current_stage = (current_stage + 1) & 3;
+			wait_time = 0.0;
+			tilt = std::min(std::max(tilt, 0.0), 3.14159 / 2.0);
 		}
 
-		float fTwistMult = sin(dTilt);// * 3.14159 / 128.0);
-		float multp = (/*fac*/dTilt * params.audio_data.GetIntensity( )) + (1 - dTilt/*fac*/) + 0.1;
+		float twist_mult = sinf(tilt);// * 3.14159 / 128.0);
+		float multp = (tilt * params.audio_data.GetIntensity( )) + (1 - tilt) + 0.1;
 
 		obj.vertices.resize(SOURCES);
 		for(int i = 0; i < SOURCES; i++)
 		{
-			float x = cos(pdAng[i]) * pdRadius[i];
-			float y = pdYPos[i];
-			float z = -100 + (sin(pdAng[i]) * pdRadius[i]);
+			float x = cosf(source_ang[i]) * source_radius[i];
+			float y = source_y_pos[i];
+			float z = -100 + (sinf(source_ang[i]) * source_radius[i]);
 
-			float sin_t = sin(pdTilt[i] * fTwistMult);
-			float cos_t = cos(pdTilt[i] * fTwistMult);
+			float sin_t = sinf(source_tilt[i] * twist_mult);
+			float cos_t = cosf(source_tilt[i] * twist_mult);
 
 			obj.vertices[i].position.x = (y * cos_t) + (x * sin_t);
 			obj.vertices[i].position.y = (y * sin_t) + (x * cos_t);
 			obj.vertices[i].position.z = z;
 
-			pdAng[i] += params.elapsed * multp * (params.audio_data.GetIntensity( ) + 0.1) * pdSpeed[i] * 3.14159 / 180.0;
-			pdTilt[i] += (params.audio_data.GetIntensity( ) + 0.1) * params.elapsed * multp * 1 * 3.14159 / 180.0;
+			source_ang[i] += params.elapsed * multp * (params.audio_data.GetIntensity( ) + 0.1) * source_speed[i] * 3.14159 / 180.0;
+			source_tilt[i] += (params.audio_data.GetIntensity( ) + 0.1) * params.elapsed * multp * 1 * 3.14159 / 180.0;
 
-			float fTransMult = FOREGROUNDBR + ((z / -100.0) * (1 - FOREGROUNDBR));
-			float fBr = params.brightness * fTransMult / 2.0;
+			float trans_mult = FOREGROUNDBR + ((z / -100.0) * (1 - FOREGROUNDBR));
+			float br = params.brightness * trans_mult / 2.0;
 
-			obj.vertices[i].diffuse = ColorRgb::Grey(0.6 * fBr * 255.0);
-	//		alObject[i]->alpha = 128*brightness*transMult;
-	//		fvVertex++;
+			obj.vertices[i].diffuse = ColorRgb::Grey(0.6 * br * 255.0);
 		}
 		obj.flags.set( Actor::F_VALID_VERTEX_DIFFUSE );
 		obj.Calculate(params.renderer, &camera, params.elapsed);
 		return nullptr;
 	}
+
 	Error* Render(const RenderParams& params) override
 	{
 		Error* error;
@@ -134,21 +123,22 @@ public:
 		error = obj.Render(params.renderer);
 		if (error) return TraceError(error);
 
-		if(pTint != nullptr)
+		if(tint != nullptr)
 		{
 			RenderState render_state;
 			render_state.blend_mode = BlendMode::Tint;
 			render_state.depth_mode = DepthMode::Disable;
-			render_state.texture_stages[0].texture = pTint;
+			render_state.texture_stages[0].texture = tint;
 
 			params.renderer.DrawSprite(render_state, Point<int>(0, 0), params.renderer.GetViewportRect(), ColorRgb::Grey(brt * 255.0));
 		}
 		return nullptr;
 	}
+
 	Error* Reconfigure(const ReconfigureParams& params) override
 	{
 		obj.textures[0].Set(Actor::TextureType::Sprite, params.texture_library.Find(sprite_texture_class));
-		pTint = params.texture_library.Find(tint_texture_class);
+		tint = params.texture_library.Find(tint_texture_class);
 		return nullptr;
 	}
 };
