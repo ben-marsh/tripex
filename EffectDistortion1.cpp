11a336860e68a34191c2f0a0ad60854438ded4b5
Ben Marsh <6538947+ben-marsh@users.noreply.github.com>
2025-01-20 08:23:14 -0500

    Fix distortion2 coding style

--- EffectDistortion1.cpp
+++ EffectDistortion1.cpp
@@ -26,7 +26,7 @@ public:
 	bool is_bigbeat = false;
 	double bigbeat = 0.0;
 	double average = 0.0;
-	Texture *tx = nullptr;
+	Texture* tx = nullptr;
 
 	EffectDistortion1()
 		: Effect({ &background_texture_class })
@@ -38,9 +38,9 @@ public:
 		angle = 0;
 
 		int i = 0;
-		for(int x = 0; x <= GRW; x++)
+		for (int x = 0; x <= GRW; x++)
 		{
-			for(int y = 0; y <= GRH; y++)
+			for (int y = 0; y <= GRH; y++)
 			{
 				precalc_atan[i] = SPIKES * atan2((y - (GRH / 2.0)) / GRH, (x - (GRW / 2.0)) / GRW);
 				i++;
@@ -53,7 +53,7 @@ public:
 		br = params.brightness;
 		xp += 0.015/*08*/ * params.audio_data.GetDampenedBand(sensitivity, 0, 0.25f) * params.elapsed;
 		yp += 0.02/*08*/ * params.audio_data.GetDampenedBand(sensitivity, 0.25f, 0.5f) * params.elapsed;
-		t += params.elapsed * std::max(0.5, params.audio_data.GetDampenedBand(sensitivity, 2/16.0f, 5/16.0f) + (3.0 * params.audio_data.GetDampenedBand(sensitivity, 7/16.0f, 12/16.0f))) * 20 * 3.14159 / 180.0;
+		t += params.elapsed * std::max(0.5, params.audio_data.GetDampenedBand(sensitivity, 2 / 16.0f, 5 / 16.0f) + (3.0 * params.audio_data.GetDampenedBand(sensitivity, 7 / 16.0f, 12 / 16.0f))) * 20 * 3.14159 / 180.0;
 
 		angle += 1 * params.elapsed;
 		fac = 0.5 + (0.15 * cos(angle * 3.14159 / 256.0));
@@ -66,9 +66,9 @@ public:
 		ColorRgb color = ColorRgb::Grey(255.0 * params.brightness);
 
 		int i = 0;
-		for(int x = 0; x <= grid.width; x++)
+		for (int x = 0; x <= grid.width; x++)
 		{
-			for(int y = 0; y <= grid.height; y++)
+			for (int y = 0; y <= grid.height; y++)
 			{
 				fx = (x - w2) * rw;
 				fy = (y - h2) * rh;
@@ -94,7 +94,7 @@ public:
 		render_state.texture_stages[0].texture = tx;
 
 		Error* error = grid.Render(params.renderer, render_state);
-		if(error) return TraceError(error);
+		if (error) return TraceError(error);
 
 		return nullptr;
 	}
@@ -106,4 +106,4 @@ public:
 	}
 };
 
-EXPORT_EFFECT( Distortion1, EffectDistortion1 )
+EXPORT_EFFECT(Distortion1, EffectDistortion1)
--- EffectDistortion2.cpp
+++ EffectDistortion2.cpp
@@ -4,15 +4,14 @@
 #include "error.h"
 #include "BezierCurve.h"
 #include "TextureData.h"
-//#include "tripex2.h"
 
-template < bool bLight > class EffectDistortion2T : public Effect
+template<bool WITH_LIGHTS> class EffectDistortion2T : public Effect
 {
 public:
 	const TextureClass background_texture_class =
 	{
 		"Background",
-		bLight ? std::vector<const uint32*>{ tex_flesh } : std::vector<const uint32*>{ tex_eyes, tex_flesh, tex_forest }
+		WITH_LIGHTS ? std::vector<const uint32*>{ tex_flesh } : std::vector<const uint32*>{ tex_eyes, tex_flesh, tex_forest }
 	};
 
 	static const int GRW = 80;
@@ -23,21 +22,19 @@ public:
 	static const int NCENTRES = 5;
 
 	BezierCurve pb[NCENTRES];
-	double pdPos[NCENTRES];
-
-	bool fSetTexture;
+	double pb_t[NCENTRES];
 
 	VertexGrid grid;
 	float xp, yp, t, fac;
 	int angle;
-	double pdHeight[GRW][GRH];
-	float precalc_atan[(GRW + 1) * (GRH + 1)];
+	float precalc_atan[(GRW + 1) * (GRH + 1)] = { };
 
 	ColorRgb pc[NCENTRES];
-	float pdA[NCENTRES], pdAS[NCENTRES];
-	Vector3 pvPos[NCENTRES];
-	double br;
-	Texture *tx;
+	float centre_ang[NCENTRES];
+	float centre_ang_speed[NCENTRES];
+	Vector3 centre_pos[NCENTRES];
+	double br = 0.0;
+	Texture* tx = nullptr;
 
 	EffectDistortion2T()
 		: Effect({ &background_texture_class })
@@ -48,78 +45,87 @@ public:
 		fac = 1;
 		angle = 0;
 
-		for(int i = 0; i < NCENTRES; i++)
+		for (int i = 0; i < NCENTRES; i++)
 		{
-			pb[i].Create(4);// = new ZBezier(4);
-			pdPos[i] = 2 + rand() * 1.0 / RAND_MAX;
-			pdA[i] = i * 20.0 * 3.14159 / 180.0;//rand() * (3.14159 * 2.0) / RAND_MAX;
-			pdAS[i] = 0.25 * ((rand() * 40.0 / RAND_MAX) - 20) * 3.14159 / 180.0;
-			if((i % 3) == 0) pc[i] = ColorRgb(255, 255, 0);
-			else if((i % 3) == 1) pc[i] = ColorRgb(255, 0, 255);
-			else pc[i] = ColorRgb(0, 255, 255);
+			pb[i].Create(4);
+			pb_t[i] = 2 + rand() * 1.0 / RAND_MAX;
+			centre_ang[i] = i * 20.0 * 3.14159 / 180.0;
+			centre_ang_speed[i] = 0.25 * ((rand() * 40.0 / RAND_MAX) - 20) * 3.14159 / 180.0;
+
+			if ((i % 3) == 0)
+			{
+				pc[i] = ColorRgb(255, 255, 0);
+			}
+			else if ((i % 3) == 1)
+			{
+				pc[i] = ColorRgb(255, 0, 255);
+			}
+			else
+			{
+				pc[i] = ColorRgb(0, 255, 255);
+			}
 		}
 	}
+
 	Error* Calculate(const CalculateParams& params) override
 	{
 		float elapsed = params.elapsed * 1.5;
 
 		br = params.brightness;
-		xp += 0.008 * std::max(0.1f, params.audio_data.GetDampenedBand( sensitivity, 0, 0.2f ) ) * elapsed;
-		yp += 0.008 * std::max(0.1f, params.audio_data.GetDampenedBand(sensitivity, 0.15f, 0.5f ) + params.audio_data.GetBeat( ) ) * elapsed;
+		xp += 0.008 * std::max(0.1f, params.audio_data.GetDampenedBand(sensitivity, 0, 0.2f)) * elapsed;
+		yp += 0.008 * std::max(0.1f, params.audio_data.GetDampenedBand(sensitivity, 0.15f, 0.5f) + params.audio_data.GetBeat()) * elapsed;
 		t += elapsed * std::max(0.5f, params.audio_data.GetDampenedBand(sensitivity, 0, 1.0f)) * 20 * 3.14159 / 180.0;
 
 		angle += 1 * elapsed;
 		fac = 0.5 + (0.15 * cos(angle * 3.14159 / 256.0));
 
-		int x, y, i = 0;
-		for(i = 0; i < NCENTRES; i++)
+		int i = 0;
+		for (i = 0; i < NCENTRES; i++)
 		{
 			Vector3 v1, v2;
-			for(pdPos[i] += 0.02 * elapsed * (0.3 + (0.7 * params.audio_data.GetIntensity( ))); pdPos[i] >= 1.0; pdPos[i] -= 1.0)
+			for (pb_t[i] += 0.02 * elapsed * (0.3 + (0.7 * params.audio_data.GetIntensity())); pb_t[i] >= 1.0; pb_t[i] -= 1.0)
 			{
 				pb[i][0] = pb[i][3];
 				pb[i][1] = pb[i][3] + (pb[i][3] - pb[i][2]);
 				pb[i][2] = Vector3(rand() * GRW / RAND_MAX, rand() * GRH / RAND_MAX, 0);
 				pb[i][3] = Vector3(rand() * GRW / RAND_MAX, rand() * GRH / RAND_MAX, 0);
 			}
-			pdA[i] += (params.audio_data.GetIntensity( ) /*+ 0.001*/) * pdAS[i] * elapsed;
-			pvPos[i] = pb[i].Calculate(pdPos[i]);
+			centre_ang[i] += (params.audio_data.GetIntensity() /*+ 0.001*/) * centre_ang_speed[i] * elapsed;
+			centre_pos[i] = pb[i].Calculate(pb_t[i]);
 		}
 
-		float pdMax[] = { 0.3f, 0.4f, 0.5f };//min(average, 0.4) / 0.4, max(0, min(1.0, (average - 0.4) / 0.4)) }; 
-
 		i = 0;
-		float fChange = 1.0f / std::max(grid.width, grid.height);
+		float delta = 1.0f / std::max(grid.width, grid.height);
 		float grid_pos_x = xp;
-		const float fMulX = 1.0f / GRW;
-		const float fMulY = 1.0f / GRH;
-		for(x = 0; x <= grid.width; x++)
+		const float mul_x = 1.0f / GRW;
+		const float mul_y = 1.0f / GRH;
+		for (int x = 0; x <= grid.width; x++)
 		{
-			grid_pos_x += fChange;
+			grid_pos_x += delta;
 
 			float grid_pos_y = yp;
-			for(y = 0; y <= grid.height; y++)
+			for (int y = 0; y <= grid.height; y++)
 			{
-				grid_pos_y += fChange;
+				grid_pos_y += delta;
 
-				float fBr = 0;
-				float fX = grid_pos_x;
-				float fY = grid_pos_y;
+				float br = 0;
+				float xp = grid_pos_x;
+				float yp = grid_pos_y;
 
 				WideColorRgb cCol(0, 0, 0);
-				for(int j = 0; j < NCENTRES; j++)
+				for (int j = 0; j < NCENTRES; j++)
 				{
-					float pos_x = x - pvPos[j].x;
-					float pos_y = y - pvPos[j].y;
-					float norm_x = pos_x * fMulX;//(1.0f / GRW);
-					float norm_y = pos_y * fMulY;//(1.0f / GRH);
+					float pos_x = x - centre_pos[j].x;
+					float pos_y = y - centre_pos[j].y;
+					float norm_x = pos_x * mul_x;//(1.0f / GRW);
+					float norm_y = pos_y * mul_y;//(1.0f / GRH);
 
 					float fRad = std::min(1.0, 100.0 / fabs(pos_x * pos_x + pos_y * pos_y));
-					fBr += fRad;
+					br += fRad;
 					float fAng = atan2(norm_y, norm_x);
-					float fLen = fRad * (0.75 + (0.5 * cos((fAng * NSPIKES) + t + pdA[j])));
-					fX += fLen * norm_x;
-					fY += fLen * norm_y;
+					float fLen = fRad * (0.75 + (0.5 * cos((fAng * NSPIKES) + t + centre_ang[j])));
+					xp += fLen * norm_x;
+					yp += fLen * norm_y;
 
 					float fColMult = params.brightness * 0.5 * std::max(0.0f, 1.0f - (sqrtf(pos_x * pos_x + pos_y * pos_y) / 30.0f));
 					cCol.r += pc[j].r * fColMult;
@@ -127,15 +133,16 @@ public:
 					cCol.b += pc[j].b * fColMult;
 				}
 
-				grid.vertices[i].tex_coords[0].x = fX;
-				grid.vertices[i].tex_coords[0].y = fY;
-				ColorRgb cGrey = ColorRgb::Grey(std::min(1.0f, params.brightness * fBr) * 255.0);
-				if(bLight) 
+				grid.vertices[i].tex_coords[0].x = xp;
+				grid.vertices[i].tex_coords[0].y = yp;
+
+				ColorRgb cGrey = ColorRgb::Grey(std::min(1.0f, params.brightness * br) * 255.0);
+				if (WITH_LIGHTS)
 				{
-					grid.vertices[i].specular = cCol * params.brightness * fBr * 0.5;
-					grid.vertices[i].diffuse =  ColorRgb::Blend(cGrey, (ColorRgb)cCol, 0.2f);//ZColour::Grey(255);
+					grid.vertices[i].specular = cCol * params.brightness * br * 0.5;
+					grid.vertices[i].diffuse = ColorRgb::Blend(cGrey, (ColorRgb)cCol, 0.2f);//ZColour::Grey(255);
 				}
-				else 
+				else
 				{
 					grid.vertices[i].diffuse = cGrey;
 					grid.vertices[i].specular = ColorRgb::Black();
@@ -143,9 +150,10 @@ public:
 				i++;
 			}
 		}
-		
+
 		return nullptr;
 	}
+
 	Error* Render(const RenderParams& params) override
 	{
 		RenderState render_state;
@@ -155,10 +163,11 @@ public:
 		render_state.texture_stages[0].texture = tx;
 
 		Error* error = grid.Render(params.renderer, render_state);
-		if(error) return TraceError(error);
+		if (error) return TraceError(error);
 
 		return nullptr;
 	}
+
 	Error* Reconfigure(const ReconfigureParams& params) override
 	{
 		tx = params.texture_library.Find(background_texture_class);
@@ -167,11 +176,5 @@ public:
 };
 
 EXPORT_EFFECT(Distortion2, EffectDistortion2T< false >)
-EXPORT_EFFECT( Distortion2Col, EffectDistortion2T< true > )
-
-//	typedef ZEffectDistortion2T<true> ZEffectDistortion2Col;
-//	DECLARE_EFFECT_PTR(ZEffectDistortion2Col, pEffectDistortion2Col)
-//
-//	typedef ZEffectDistortion2T<false> ZEffectDistortion2;
-//	DECLARE_EFFECT_PTR(ZEffectDistortion2, pEffectDistortion2)
+EXPORT_EFFECT(Distortion2Col, EffectDistortion2T< true >)
 
