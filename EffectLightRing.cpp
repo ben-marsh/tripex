d3bbb5cd83d3656c54a8d9b97ab46e2c8397b45e
Ben Marsh <6538947+ben-marsh@users.noreply.github.com>
2025-01-20 08:40:44 -0500

    Coding style changes for lightring

--- EffectLightRing.cpp
+++ EffectLightRing.cpp
@@ -5,13 +5,6 @@
 #include "TextureData.h"
 #include "VertexGrid.h"
 
-//static ZObject::Transformed *pFirst;
-
-//static double linearity = 0;
-
-//static unsigned int buffersize = 0;
-//static D3DTLVERTEX *vb = nullptr;
-
 class EffectLightRing : public Effect
 {
 public:
@@ -36,27 +29,23 @@ public:
 	Vector3 ls[SOURCES][4];
 	double position[SOURCES];
 	double speed[SOURCES];
-	double length[FRAMES][SOURCES];
+	double length[FRAMES][SOURCES] = { };
 	double er[SOURCES];
 	double ehp[SOURCES];
 	double ehps[SOURCES];
-	bool fNotRendered;
 	double accum, r, p, y;
-	double brt;
-	Texture *ptTint;
+	double brt = 0;
+	double a2 = 0;
+	Texture *tint = nullptr;
 	Actor obj;
 	Camera camera;
 
-	Texture *tex;
-	bool fSetTexture;
-	Texture *tx;
-	double br;
+	Texture * texture = nullptr;
+	double br = 0;
 
 	EffectLightRing()
 		: Effect({ &sprite_texture_class, &tint_texture_class })
 	{
-		fSetTexture = true;
-		fNotRendered = true;
 		accum = 0;
 		r = p = y = 0;
 
@@ -83,6 +72,7 @@ public:
 	
 		obj.vertices.resize(SOURCES);
 	}
+
 	Error* Calculate(const CalculateParams& params) override
 	{
 		brt = params.brightness;
@@ -96,31 +86,31 @@ public:
 				position[i] += 0.25 * speed[i] * params.audio_data.GetIntensity( ) * elapsed;
 				ehp[i] += 0.25 * ehps[i] * (0.4 + 2 * (params.audio_data.GetIntensity( ) + params.audio_data.GetBeat( ))) * elapsed;
 
-				double r = 2.0 * er[i] * cos(ehp[i]);
-
-				obj.vertices[i].position.x = (46.0 + (r * sin(ehp[i]))) * sin(position[i]);
-				obj.vertices[i].position.z = (46.0 + (r * cos(ehp[i]))) * cos(position[i]);
+				double rad = 2.0 * er[i] * cos(ehp[i]);
+				obj.vertices[i].position.x = (46.0 + (rad * sin(ehp[i]))) * sin(position[i]);
+				obj.vertices[i].position.z = (46.0 + (rad * cos(ehp[i]))) * cos(position[i]);
 				obj.vertices[i].position.y = er[i] * sin(ehp[i]);	
 			}
 	
-			static double a2 = 0;
 			a2 += elapsed * (params.audio_data.GetIntensity( ) + params.audio_data.GetBeat( )) * 3.14159 / 180.0;
 
 			obj.roll += elapsed * 0.25 * params.audio_data.GetIntensity( ) * 4.0 * 3.14159 / 180.0;
 			obj.pitch += elapsed * 0.25 * params.audio_data.GetIntensity( ) * 3.0 * 3.14159 / 180.0;
 			obj.yaw += elapsed * 0.25 * 2.0 * 3.14159 / 180.0;
 
-			obj.textures[0].Set(Actor::TextureType::Sprite, tx);
+			obj.textures[0].Set(Actor::TextureType::Sprite, texture);
 			obj.Calculate(params.renderer, &camera, elapsed);
 		}
 		return nullptr;
 	}
+
 	Error* Reconfigure(const ReconfigureParams& params) override
 	{
-		tx = params.texture_library.Find(sprite_texture_class);
-		ptTint = params.texture_library.Find(tint_texture_class);
+		texture = params.texture_library.Find(sprite_texture_class);
+		tint = params.texture_library.Find(tint_texture_class);
 		return nullptr;
 	}
+
 	Error* Render(const RenderParams& params) override
 	{
 		Error* error;
@@ -128,20 +118,21 @@ public:
 		error = obj.Render(params.renderer);
 		if(error) return TraceError(error);	
 
-		if(ptTint != nullptr)
+		if(tint != nullptr)
 		{
 			RenderState render_state;
 			render_state.blend_mode = BlendMode::Tint;
 			render_state.depth_mode = DepthMode::Disable;
-			render_state.texture_stages[0].texture = ptTint;
+			render_state.texture_stages[0].texture = tint;
 
 			params.renderer.DrawSprite(render_state, Point<int>(0, 0), Rect<int>(0, 0, params.renderer.GetWidth(), params.renderer.GetHeight()), ColorRgb::Grey(brt * 255.0));
 		}
 		return nullptr;
 	}
-	bool CanRender(double dElapsed)
+
+	bool CanRender(double elapsed)
 	{
-		return (dElapsed > 0.1);//1.0);
+		return (elapsed > 0.1);//1.0);
 	}
 };
 
