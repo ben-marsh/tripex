a64a4eadf08f1de18c69cc7f9a422a1192e5a28e
Ben Marsh <6538947+ben-marsh@users.noreply.github.com>
2025-01-19 21:53:44 -0500

    Remove unused color methods.

--- ColorRgb.h
+++ ColorRgb.h
@@ -31,15 +31,9 @@ public:
 	}
 
 	// MakeFloat( ):
-	static BaseColorRgb<T> MakeFloat(float fR, float fG, float fB){ return BaseColorRgb<T>(Bound<int>((int)(255.0f * fR), 0, 255), Bound<int>((int)(255.0f * fG), 0, 255), Bound<int>((int)(255.0f * fB), 0, 255)); }
-	static BaseColorRgb<T> MakeBGR(unsigned int dw){ return BaseColorRgb<T>((unsigned char)dw, (unsigned char)(dw >> 8L), (unsigned char)(dw >> 16L)); }
-	static BaseColorRgb<T> MakeRGB(unsigned int dw){ return BaseColorRgb<T>((unsigned char)(dw >> 16L), (unsigned char)(dw >> 8L), (unsigned char)dw); }
 	static BaseColorRgb<T> White(){ return BaseColorRgb<T>(255, 255, 255); }
 	static BaseColorRgb<T> Black(){ return BaseColorRgb<T>(0, 0, 0); }
 	static BaseColorRgb<T> Grey(int g){ g = Bound<int>(g, 0, 255); return BaseColorRgb<T>((T)g, (T)g, (T)g); }
-
-	// Set( ):
-	void Set( T nR, T nG, T nB );
 
 	// const operators
 	BaseColorRgb<T> operator+() const { return *this; }
@@ -54,25 +48,14 @@ public:
 	template < class U > bool operator!=(const BaseColorRgb<U> &c) const { return !operator==(c); }
 	template < class U > operator BaseColorRgb<U>() const { BaseColorRgb<unsigned char> c; SetCastedColourData(c, *this); return c; }
 
-	uint32 GetBGR() const { BaseColorRgb<unsigned char> c = *this; return (((c.m_nB << 8) + c.m_nG) << 8) + c.m_nR; }
-	uint32 GetRGB() const { BaseColorRgb<unsigned char> c = *this; return (((c.m_nR << 8) + c.m_nG) << 8) + c.m_nB; }
-	uint32 GetBGRA() const { return (GetBGR() << 8) | 0xff; }
-	uint32 GetARGB() const { return GetRGB() | 0xff000000; }
-	COLORREF GetCOLORREF() const { return GetBGR(); }
-
 	// operators
 	template < class U > BaseColorRgb<T> &operator+=(const BaseColorRgb<U> &c){ return operator=(operator+(c)); }
 	template < class U > BaseColorRgb<T> &operator-=(const BaseColorRgb<U> &c){ return operator=(operator-(c)); }
 	template < class U > BaseColorRgb<T> &operator*=(U n){ return operator=(operator*(n)); }
 	template < class U > BaseColorRgb<T> &operator/=(U n){ return operator=(operator/(n)); }
-	BaseColorRgb<T> &operator>>=(int n){ return operator=(operator>>(n)); }
-	BaseColorRgb<T> &operator<<=(int n){ return operator=(operator<<(n)); }
 	template < class U > BaseColorRgb<T> &operator=(const BaseColorRgb<U> &c){ SetCastedColourData<T, U>(*this, c); return *this; }
 
 	// other
-	BaseColorRgb<T> Reverse() const { return BaseColorRgb<T>(b, g, r); }
-	BaseColorRgb<T> Inverse() const { return BaseColorRgb<T>(255 - r, 255 - g, 255 - b); }
-	void Invert(){ operator=(Inverse()); }
 	int GetMaxComponent() const { return std::max(r, std::max(g, b)); }
 	int GetMinComponent() const { return std::min(r, std::min(g, b)); }
 	static BaseColorRgb<T> Blend(const BaseColorRgb<T> &c1, const BaseColorRgb<T> &c2, float fBlend){ return (BaseColorRgb<T>)((c1 * (1.0f - fBlend)) + (c2 * fBlend)); }
@@ -112,21 +95,7 @@ template< class T > BaseColorRgb< T >::BaseColorRgb( float fBr )
 
 template< class T > BaseColorRgb< T >::BaseColorRgb( T nR, T nG, T nB )
 {
-	Set(nR, nG, nB);
-}
-
-//template< class T > ZBaseColour< T >::template < class U > ZBaseColour( const ZBaseColour<U> &c )
-//{
-//	SetCastedColourData(*this, c);
-//}
-
-/*---------------------------------
-* Set( )
------------------------------------*/
-
-template< class T > void BaseColorRgb< T >::Set( T nR, T nG, T nB )
-{
 	r = nR;
-	g = nG; 
+	g = nG;
 	b = nB;
 }
